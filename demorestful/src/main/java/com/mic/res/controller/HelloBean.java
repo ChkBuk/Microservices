79daba8da6f570ac9115fd7df21a207592e6c482
Charith <cbkulawardana@gmail.com>
2022-12-10 11:54:00 +0800

    Created REST API with path variables

---
+++ demorestful/src/main/java/com/mic/res/controller/HelloBean.java
@@ -0,0 +1,19 @@
+package com.mic.res.controller;
+
+public class HelloBean {
+    private String message;
+    public HelloBean(String message) {
+        this.message = message;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    @Override
+    public String toString() {
+        return "HelloBean [message=" + message + "]";
+    }
+}
--- demorestful/src/main/java/com/mic/res/controller/RestExample.java
+++ demorestful/src/main/java/com/mic/res/controller/RestExample.java
@@ -1,9 +1,15 @@
 package com.mic.res.controller;
 
+import javax.naming.Name;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.sym.Name1;
+import com.fasterxml.jackson.core.sym.Name3;
 @RestController
 public class RestExample {
    // @RequestMapping(method = RequestMethod.GET,path="/hello")
@@ -11,4 +17,15 @@ public class RestExample {
     public String showMessage(){
         return "Hellow..!";
     }
+    @GetMapping(path="/hello-bean")
+    public HelloBean showMessageBean(){
+        return new HelloBean("Hellow Bean");
+    }
+
+    @GetMapping(path="/hello-bean/path_variables/{name}")
+    public HelloBean showMessagePathVariable(@PathVariable String name){
+        return new HelloBean(String.format("Hi %s",name));
+    }
+   
+   
 }
